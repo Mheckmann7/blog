6eee8171f332218822716c1c195b613ad6636c15
Mira Heckmann <miraheckmann@gmail.com>
2021-02-24 18:16:45 -0800

    adds nav and post model

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    body = models.TextField(max_length=1000)
+    date = models.DateField('date created')
+    
+    def __str__(self):
+        return self.title
