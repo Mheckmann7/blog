aa2dae251339233d622e12e233aff7852c687533
Alyssa Karakula <alyssakarakula@Alyssas-MacBook-Air.local>
2021-02-26 22:29:00 -0500

    creates post form

--- main_app/views.py
+++ main_app/views.py
@@ -5,3 +5,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'home.html')
+
+def posts_index(request):
+    return render(request, 'posts/index.html', { 'posts': posts })
