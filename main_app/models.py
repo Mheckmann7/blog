203e2c761d7bc2715c63edf9c72e4a7925a96620
Mira Heckmann <miraheckmann@gmail.com>
2021-03-02 16:52:29 -0800

    adds auth

--- main_app/models.py
+++ main_app/models.py
@@ -1,12 +1,15 @@
 from django.db import models
 from django.urls import reverse
 # Create your models here.
+from django.contrib.auth.models import User
 
 
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=1000)
     date = models.DateField('date created')
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
--- main_app/views.py
+++ main_app/views.py
@@ -1,7 +1,26 @@
 from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Post
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
+
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('posts_index')
+        else:
+            error_message = 'Invalid sign up - try again'
+    form = UserCreationForm()
+    context = {'form': form, 'error_message': error_message}
+    return render(request, 'registration/signup.html', context)
 
 
 def home(request):
@@ -9,11 +28,13 @@ def home(request):
     return render(request, 'home.html', {'posts': posts})
 
 
+@login_required
 def posts_index(request):
-    posts = Post.objects.all()
+    posts = Post.objects.filter(user=request.user)
     return render(request, 'posts/index.html', {'posts': posts})
 
 
+@login_required
 def posts_detail(request, post_id):
     post = Post.objects.get(id=post_id)
     # post_form = PostForm()s
@@ -21,17 +42,21 @@ def posts_detail(request, post_id):
     return render(request, 'posts/detail.html', {'post': post})
 
 
-class PostCreate(CreateView):
+class PostCreate(LoginRequiredMixin, CreateView):
     model = Post
     fields = ['title', 'body', 'date']
     success_url = '/posts/'
 
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
 
-class PostUpdate(UpdateView):
+
+class PostUpdate(LoginRequiredMixin, UpdateView):
     model = Post
     fields = ['title', 'body']
 
 
-class PostDelete(DeleteView):
+class PostDelete(LoginRequiredMixin, DeleteView):
     model = Post
     success_url = '/posts/'
