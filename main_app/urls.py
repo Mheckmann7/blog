36c9661d2fbbb6ca7cfa75dcbf58e4fca78ea285
Mira Heckmann <miraheckmann@gmail.com>
2021-03-03 18:31:45 -0800

    adds comments

--- main_app/urls.py
+++ main_app/urls.py
@@ -10,4 +10,10 @@ urlpatterns = [
     path('posts/<int:pk>/update', views.PostUpdate.as_view(), name='posts_update'),
     path('posts/<int:pk>/delete', views.PostDelete.as_view(), name='posts_delete'),
     path('accounts/signup/', views.signup, name='signup'),
+    path('posts/<int:post_id>/add_comment/',
+         views.add_comment, name='add_comment'),
+    path('comments/<int:pk>/update/',
+         views.CommentUpdate.as_view(), name='comments_update'),
+    path('comments/<int:pk>/delete/',
+         views.CommentDelete.as_view(), name='comments_delete'),
 ]
