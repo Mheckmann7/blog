4a95561b40edf4986228082b8395e32a091843b2
Mira Heckmann <miraheckmann@gmail.com>
2021-03-09 19:00:34 -0800

    fixes comments adds styling

--- main_app/models.py
+++ main_app/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 class Post(models.Model):
     title = models.CharField(max_length=100)
     body = models.TextField(max_length=20000)
-    date = models.DateField('date created')
+    date = models.DateField(default=datetime.now, blank=True)
     is_favorite = models.BooleanField(default=False)
 
     user = models.ForeignKey(User, on_delete=models.CASCADE)
@@ -24,6 +24,8 @@ class Comment(models.Model):
     body = models.TextField(max_length=2000)
     date = models.DateField(default=datetime.now, blank=True)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.body
